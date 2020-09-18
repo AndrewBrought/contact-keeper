01e2a04e50a94616f4b09eeb1eba28bbc1394be3
Andrew Brought <andrewbrought@gmail.com>
2020-09-18 15:34:26 -0500

    built out alert context, state, and component

---
+++ client/src/components/layout/Alerts.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+const Alerts = () => {
+    const alertContext = useContext(AlertContext);
+
+    return (
+       //  It's going to look at the alerts in the context in our state, if there is anything
+        //  it's going to loop through them and output this jsx
+
+       alertContext.alerts.length > 0 && alertContext.alerts.map(alert => (
+           <div key={alert.id} className={`alert alert-${alert.type}`}>
+                <i className="fas fa-info-circle" /> {alert.msg}
+           </div>
+       ))
+    );
+}
+
+export default Alerts;
