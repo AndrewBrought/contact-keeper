96549271928c0d0327e2db61cd00b8f5ef97c2d9
Andrew Brought <andrewbrought@gmail.com>
2020-09-17 20:14:17 -0500

    finished contact form and state, finishing add and delete animation...was hung up on a very silly syntax error concerning text for way too long

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,15 +1,17 @@
 import React, {useContext, useEffect, useRef} from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
+
+
 const ContactFilter = () => {
     const contactContext = useContext(ContactContext);
-    const text = useRef('');
+   const text = useRef('');
 
     const { filterContacts, clearFilter, filtered } = contactContext;
 
     useEffect(() => {
-       if(filtered === null) {
-           text.current.value = '';
+       if (filtered === null) {
+           text.current = '';
        }
     });
 
@@ -21,12 +23,12 @@ const ContactFilter = () => {
         } else {
             clearFilter();
         }
-    }
+    };
 
 
     return (
         <form>
-            <input ref='text' type='text' placeholder='Filter Contacts...' onChange={onChange}/>
+            <input ref={text} type='text' placeholder='Filter Contacts...' onChange={onChange}/>
         </form>
     )
 }
--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,25 +1,34 @@
-import React, { Fragment, useContext } from 'react';
+import React, {Fragment, useContext} from 'react';
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 import ContactItem from "./ContactItem";
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
-    const { contacts, filtered } = contactContext;
+    const {contacts, filtered} = contactContext;
 
-    if(contacts.length === 0) {
-       return <h4>Please add a contact</h4>
+    if (contacts.length === 0) {
+        return <h4>Please add a contact</h4>
     }
 
     return (
         <Fragment>
-            {/*We're pulling filtered and contacts out, checking if there's anything in filtered,
+            <TransitionGroup>
+                {/*We're pulling filtered and contacts out, checking if there's anything in filtered,
             if there is (!== null), then we map filtered and show the contact item...if there's nothing
             in filtered, then we're going to show the contacts*/}
-            {filtered !== null ? filtered.map(contact =>  (<ContactItem key={contact.id}
-           contact={contact}/>)) : contacts.map(contact => (
-                <ContactItem key={contact.id} contact={contact}/>
-            ))}
+                {filtered !== null ? filtered.map(contact => (
+                        <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                            <ContactItem contact={contact}/>
+                        </CSSTransition>
+                    ))
+                    : contacts.map(contact => (
+                        <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                            <ContactItem contact={contact}/>
+                        </CSSTransition>
+                    ))}
+            </TransitionGroup>
         </Fragment>
     )
 }
