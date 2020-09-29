88d39b4b62f84a73926d3bd2c70a44d70b2add0a
Andrew Brought <andrewbrought@gmail.com>
2020-09-29 16:46:36 -0500

    redeploying

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -64,7 +64,6 @@ const ContactItem = ({ contact }) => {
     //     return <a href={`instagram://user?username=${instagram}`}>{instagram}</a>;
     // }
 
-
       // return <a href={openAppOrStore}> {instagram}</a>
    }
 
