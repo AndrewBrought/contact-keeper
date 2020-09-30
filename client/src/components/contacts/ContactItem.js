46c9cdc37a1611d859c9dd0fd679f599b88b4739
Andrew Brought <andrewbrought@gmail.com>
2020-09-30 09:57:52 -0500

    switched the function handling, trying a more simple approach

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -34,128 +34,117 @@ const ContactItem = ({contact}) => {
             let iosLink = `instagram://user?username=${instagram}`
 
             const openAppOrStore = () => {
-                setTimeout(function () {
-                    if (!openedApp) {
-                        // iosLink = "apps.apple.com/us/app/instagram/id389801252";
-                        iosLink = 'https://www.instagram.com/';
-                    }
-                }, 25);
-                // const iosLink = `instagram://user?username=${instagram}`;
-                try {
-                    window.location = iosLink;
-                    if (window.location.href.indexOf("instagram://") !== -1) {
-                        openedApp = true;
-                    }
-                } catch (e) {
+
+                // iosLink = "apps.apple.com/us/app/instagram/id389801252";
+                // iosLink = 'https://www.instagram.com/';
+
+
+                    let app = {
+                        launchApp: function () {
+                            window.location.replace(`instagram://user?username=${instagram}`);
+                            this.timer = setTimeout(this.openWebApp, 1000);
+                        },
+
+                        openWebApp: function () {
+                            window.location.replace("apps.apple.com/us/app/instagram/id389801252");
+                        }
+                    };
+
+                    app.launchApp();
+                }
+                    return <a href={openAppOrStore()} target="_blank"> {instagram}</a>
+            }
+
+            // const tagLinkFacebook = () => {
+            //     if (OSName === "iOS") {
+            //         return <a href={`facebook://user?username=${facebook}`}> {facebook}</a>;
+            //     } else if (OSName === "MacOS") {
+            //         return <a href={`https://www.facebook.com/${facebook}`} target="_blank"> {facebook}</a>;
+            //     } else if(OSName === "Windows") {
+            //         return <a href={`https://www.facebook.com/${facebook}`} target="_blank"> {facebook}</a>;
+            //     } else if(OSName === "Android") {
+            //         return <a href={`facebook://user?username=${facebook}`}> {facebook}</a>;
+            //     }
+            // }
+
+            // const tagLinkTwitter = () => {
+            //     if (OSName === "iOS") {
+            //         return <a href={`twitter://user?username=${twitter}`}> {twitter}</a>;
+            //     } else if (OSName === "MacOS") {
+            //         return <a href={`https://www.twitter.com/${twitter}`} target="_blank"> {twitter}</a>;
+            //     } else if(OSName === "Windows") {
+            //         return <a href={`https://www.twitter.com/${twitter}`} target="_blank"> {twitter}</a>;
+            //     } else if(OSName === "Android") {
+            //         return <a href={`twitter://user?username=${twitter}`}> {twitter}</a>;
+            //     }
+            // }
+
+            const tagLinkLinedIn = () => {
+                if (OSName === "iOS") {
+                    return <a href={`linkedin://user?username=${linkedIn}`}> {linkedIn}</a>;
+                } else if (OSName === "MacOS") {
+                    return <a href={`https://www.linkedin.com/in/${linkedIn}`} target="_blank"> {linkedIn}</a>;
+                } else if (OSName === "Windows") {
+                    return <a href={`https://www.linkedin.com/in/${linkedIn}`} target="_blank"> {linkedIn}</a>;
+                } else if (OSName === "Android") {
+                    return <a href={`linkedin://user?username=${linkedIn}`}> {linkedIn}</a>;
                 }
             }
 
-            const parts = window.location.href.split('/');
-            const consultationId = parts[parts.length - 2];
-            setTimeout(function () {
-                if (window.location.href.indexOf(consultationId) !== -1) {
-                    if (OSName === "MacOS") {
-                        openAppOrStore();
-                    }
-                }
-            }, 25);
-            return <a href={iosLink}> {instagram}</a>
+            return (
+                <div className='card bg-light'>
+                    <h3 className="text-primary text-left">
+                        {name}{' '} <span style={{float: 'right'}}
+                                          className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+                    </h3>
+                    <ul className='list'>
+
+                        {email && (<li>
+                            <i className="fas fa-envelope-open"/> <a href={`mailto:${email}`}>{email}</a>
+                        </li>)}
+
+                        {phone && (<li>
+                            <i className="fas fa-phone"/> <a href={`tel:${phone}`}>{phone}</a>
+                        </li>)}
+
+                        {linkedIn && (<li>
+                            <i className="fab fa-linkedin"/>
+                            {tagLinkLinedIn()}
+                            {/*<a href={`https://l.linklyhq.com/l/7PG7${linkedIn}`} target="_blank"> {linkedIn}</a>*/}
+
+                        </li>)}
+
+                        {facebook && (<li>
+                            <i className="fab fa-facebook-square"/>
+
+                            {/*{tagLinkFacebook()}*/}
+                            <a href={`https://www.facebook.com/${facebook}`} target="_blank"> {facebook}</a>
+                        </li>)}
+
+                        {instagram && (<li>
+                            <i className="fab fa-instagram"/>
+                            {tagLinkInstagram()}
+
+                        </li>)}
+
+                        {twitter && (<li>
+                            <i className="fab fa-twitter-square"/>
+                            {/*{tagLinkTwitter()}*/}
+                            <a href={`https://www.twitter.com/${twitter}`} target="_blank"> {twitter}</a>
+                        </li>)}
+
+                    </ul>
+                    <p>
+                        <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
+                        <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
+                    </p>
+                </div>
+            );
+        }
+        ;
+
+        ContactItem.propTypes = {
+            contact: PropTypes.object.isRequired,
         }
 
-        // else if(OSName === "Android") {
-        //     return <a href={`instagram://user?username=${instagram}`}>{instagram}</a>;
-        // }
-    }
-
-    // const tagLinkFacebook = () => {
-    //     if (OSName === "iOS") {
-    //         return <a href={`facebook://user?username=${facebook}`}> {facebook}</a>;
-    //     } else if (OSName === "MacOS") {
-    //         return <a href={`https://www.facebook.com/${facebook}`} target="_blank"> {facebook}</a>;
-    //     } else if(OSName === "Windows") {
-    //         return <a href={`https://www.facebook.com/${facebook}`} target="_blank"> {facebook}</a>;
-    //     } else if(OSName === "Android") {
-    //         return <a href={`facebook://user?username=${facebook}`}> {facebook}</a>;
-    //     }
-    // }
-
-    // const tagLinkTwitter = () => {
-    //     if (OSName === "iOS") {
-    //         return <a href={`twitter://user?username=${twitter}`}> {twitter}</a>;
-    //     } else if (OSName === "MacOS") {
-    //         return <a href={`https://www.twitter.com/${twitter}`} target="_blank"> {twitter}</a>;
-    //     } else if(OSName === "Windows") {
-    //         return <a href={`https://www.twitter.com/${twitter}`} target="_blank"> {twitter}</a>;
-    //     } else if(OSName === "Android") {
-    //         return <a href={`twitter://user?username=${twitter}`}> {twitter}</a>;
-    //     }
-    // }
-
-    const tagLinkLinedIn = () => {
-        if (OSName === "iOS") {
-            return <a href={`linkedin://user?username=${linkedIn}`}> {linkedIn}</a>;
-        } else if (OSName === "MacOS") {
-            return <a href={`https://www.linkedin.com/in/${linkedIn}`} target="_blank"> {linkedIn}</a>;
-        } else if (OSName === "Windows") {
-            return <a href={`https://www.linkedin.com/in/${linkedIn}`} target="_blank"> {linkedIn}</a>;
-        } else if (OSName === "Android") {
-            return <a href={`linkedin://user?username=${linkedIn}`}> {linkedIn}</a>;
-        }
-    }
-
-    return (
-        <div className='card bg-light'>
-            <h3 className="text-primary text-left">
-                {name}{' '} <span style={{float: 'right'}}
-                                  className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
-            </h3>
-            <ul className='list'>
-
-                {email && (<li>
-                    <i className="fas fa-envelope-open"/> <a href={`mailto:${email}`}>{email}</a>
-                </li>)}
-
-                {phone && (<li>
-                    <i className="fas fa-phone"/> <a href={`tel:${phone}`}>{phone}</a>
-                </li>)}
-
-                {linkedIn && (<li>
-                    <i className="fab fa-linkedin"/>
-                    {tagLinkLinedIn()}
-                    {/*<a href={`https://l.linklyhq.com/l/7PG7${linkedIn}`} target="_blank"> {linkedIn}</a>*/}
-
-                </li>)}
-
-                {facebook && (<li>
-                    <i className="fab fa-facebook-square"/>
-
-                    {/*{tagLinkFacebook()}*/}
-                    <a href={`https://www.facebook.com/${facebook}`} target="_blank"> {facebook}</a>
-                </li>)}
-
-                {instagram && (<li>
-                    <i className="fab fa-instagram"/>
-                    {tagLinkInstagram()}
-
-                </li>)}
-
-                {twitter && (<li>
-                    <i className="fab fa-twitter-square"/>
-                    {/*{tagLinkTwitter()}*/}
-                    <a href={`https://www.twitter.com/${twitter}`} target="_blank"> {twitter}</a>
-                </li>)}
-
-            </ul>
-            <p>
-                <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
-                <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
-            </p>
-        </div>
-    );
-};
-
-ContactItem.propTypes = {
-    contact: PropTypes.object.isRequired,
-}
-
-export default ContactItem;
+        export default ContactItem;
